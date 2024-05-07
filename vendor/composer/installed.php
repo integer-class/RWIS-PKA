eac84935cc8f93658e1d52df49ab4693c6379408
Ananda Az Haruddin Salima <azharuddinananda@gmail.com>
2024-05-07 14:43:14 +0700

    tes commit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v10.3.3',
-        'version' => '10.3.3.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '656d9cdae910e19096d57ee93e5faa6555277835',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v10.3.3',
-            'version' => '10.3.3.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '656d9cdae910e19096d57ee93e5faa6555277835',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
