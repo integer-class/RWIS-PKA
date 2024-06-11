8fc12e58392942085f96d533023fc1cb1354ee22
Shofwah Kanaka <Shofwahknk@gmail.com>
2024-06-11 12:27:42 +0700

    Update keuanganModelFactory.php

--- database/factories/keuanganModelFactory.php
+++ database/factories/keuanganModelFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Models\CitizenModel;
 use App\Models\UserModel;
-
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class KeuanganModelFactory extends Factory
@@ -17,8 +16,13 @@ class KeuanganModelFactory extends Factory
     public function definition(): array
     {
         // Ensure citizens are loaded once and used across calls
-        $user = userModel::where('role' ,'!=','3')->pluck('nik')->toArray();
-        $citizens = CitizenModel::where('nik',$user)->pluck('nama')->toArray();
+
+        // Modify this to join the user and citizen tables correctly
+        $users = UserModel::where('role', '!=', '3')->pluck('nik')->toArray();
+        $ketua = CitizenModel::whereIn('nik', $users)->pluck('nama')->toArray();
+        $citizen = CitizenModel::pluck('nama')->toArray();
+
+
 
         // Generate the amount for pemasukan first
         $pemasukanAmount = $this->faker->numberBetween(10, 99) * 1000;
@@ -31,14 +35,25 @@ class KeuanganModelFactory extends Factory
         
         // Set jumlah based on jenis_data
         $jumlah = $jenisData === 'pemasukan' ? $pemasukanAmount : $pengeluaranAmount;
+        
+        //i want if the jenis_data is pemasukan then nama is $citizen
+        //i want if the jenis_data is pengeluaran then nama is $ketua
+        $nama = $jenisData === 'pemasukan' ? $this->faker->randomElement($citizen) : $this->faker->randomElement($ketua);
 
         return [
             'tanggal' => $this->faker->dateTimeBetween('2024-01-01', 'now'),
             'jenis_iuran' => $this->faker->randomElement(['iuran PHB', 'iuran kematian', 'iuran sampah', 'iuran listrik']),
             'jenis_data' => $jenisData,
             'jumlah' => $jumlah, // 5 digit numbers but in thousands
-            'nama' => $this->faker->randomElement($citizens), // Randomize from nama column from citizen table
+            'nama' => $nama, // Randomize from nama column from citizen table
             'rt' => $this->faker->randomElement(['1', '2', '3']),
         ];
     }
 }
+
+            // 'tanggal' 
+            // 'jenis_iuran'
+            // 'jenis_data' 
+            // 'jumlah' 
+            // 'nama' 
+            // 'rt'
